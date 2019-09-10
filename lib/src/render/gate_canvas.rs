4cbeda6a170f21e01b5ab613549515f193d0d0ed
Austin Jones <austin.jones@aetion.com>
2019-09-09 23:19:55 -0400

    Run cargo fix

--- lib/src/render/gate_canvas.rs
+++ lib/src/render/gate_canvas.rs
@@ -1,21 +1,21 @@
-use std::cell::RefCell;
-use std::ops::DerefMut;
-use std::rc::Rc;
 
-use luminance::framebuffer::ColorSlot;
-use luminance::framebuffer::Framebuffer;
-use luminance::pixel::{R32F, R8UI, RG32F, RG8UI, RGBA32F};
-use luminance::render_state::RenderState;
-use luminance::tess::{Mode, TessBuilder};
-use luminance::texture::{Dim2, Flat, GenMipmaps, Sampler, Texture};
+
+
+
+
+
+
+
+
+
 use std::iter::Iterator;
 
 use crate::colormap::ColormapHandle;
 use crate::render::gate_layer::LayerGate;
-use crate::render::semantics_stipple::Vertex;
-use crate::texture::{TextureHandle, TextureRenderer};
-use image::{DynamicImage, LumaA};
-use luminance::context::GraphicsContext;
+
+
+
+
 
 /// CanvasGate represents an start-to-finish render to a Framebuffer.
 /// Manages high-level resources such as Color Maps, Textures, and Layers.
